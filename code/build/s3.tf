849d2e58e480dd27f32120f04de21ab20c1411cc
k-laughman <laughmk@gmail.com>
2024-09-18 15:08:26 -0400

    update to s3 bucket creation tfc

---
+++ code/build/s3.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "devsecops_s3" {
+  bucket_prefix = "devsecops-"
+
+  tags = {
+    Environment      = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "devsecops_s3" {
+  bucket = aws_s3_bucket.dev_s3.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
