3d3c4ce9ce9e58a054f11bd1abe122bc40bc7698
k-laughman <laughmk@gmail.com>
2024-09-18 15:16:37 -0400

    updated s3_tfc code

--- code/build/s3.tf
+++ code/build/s3.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "devsecops_s3" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "devsecops_s3" {
-  bucket = aws_s3_bucket.dev_s3.id
+  bucket = aws_s3_bucket.devsecops_s3.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
